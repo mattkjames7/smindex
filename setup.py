cc6101705f12bb913f5aff15b669ece2822f850f
Dr Matt James <mattkjames7@gmail.com>
2026-01-21 00:08:14 +0000

    Workflow fixes (#2)

--- setup.py
+++ setup.py
@@ -1,29 +1,5 @@
 import setuptools
-import os
-
-
-def getversion():
-    """
-    read the version string from __init_.py
-    """
-    # get the init file path
-    thispath = os.path.abspath(os.path.dirname(__file__))+"/"
-    initfile = thispath + "smindex/__init__.py"
-
-    # read the file in
-    f = open(initfile, "r")
-    lines = f.readlines()
-    f.close()
-
-    # search for the version
-    version = "unknown"
-    for item in lines:
-        if "__version__" in item:
-            s = item.split("=")
-            version = s[-1].strip().strip('"').strip("'")
-            break
-    return version
-
+from get_version import get_version
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -31,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="smindex",
-    version=getversion(),
+    version=get_version(),
     author="Matthew Knight James",
     author_email="mattkjames7@gmail.com",
     description="A tiny module for reading SuperMAG indices and substorm lists.",
