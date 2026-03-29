c6f0cae535a7d777d21edb5ce7a8f84484e9ac14
Matt James <mattkjames7@gmail.com>
2026-03-29 12:48:42 +0100

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 from get_version import get_version
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 
